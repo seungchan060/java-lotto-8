c5e024b17926c266ebc6ab71fb4a99d1b547cf46
SeungChan <dlatmdcks60@gmail.com>
2025-10-30 18:33:53 +0900

    feat(InputView): 당첨 번호 및 보너스 번호 입력을 위한 메서드 구현
    - readWinningNumbers(): 사용자에게 당첨 번호 입력을 요청하고 반환
    - readBonusNumber(): 사용자에게 보너스 번호 입력을 요청하고 반환

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -7,4 +7,14 @@ public class InputView {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
+
+    public String readWinningNumbers() {
+        System.out.println("\n당첨 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public String readBonusNumber() {
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
 }
