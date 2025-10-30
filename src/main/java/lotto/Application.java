4f7aac2669a413f131937ee85d987a6fe380d540
SeungChan <dlatmdcks60@gmail.com>
2025-10-30 16:41:34 +0900

    feat(LottoValidator): 구입 금액 검증
    - 최소 구입 금액 1000
    - 1000단위의 구입 금액

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,7 +3,8 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
-        String purchaseAmount = inputView.readPurchaseAmount();
+        String raw = inputView.readPurchaseAmount();
+        int purchaseAmount = LottoValidator.validatePurchaseAmount(raw);
 
     }
 }
--- src/main/java/lotto/LottoValidator.java
+++ src/main/java/lotto/LottoValidator.java
@@ -3,6 +3,7 @@ package lotto;
 public class LottoValidator {
     public static int validatePurchaseAmount(String raw) {
         int amount = parseInt(raw);
+        validateAmount(amount);
         return amount;
     }
 
@@ -12,6 +13,11 @@ public class LottoValidator {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 정수여야 합니다.");
         }
+    }
+
+    private static void validateAmount(int amount) {
+        if (amount < 1000) throw new IllegalArgumentException("[ERROR] 최소 구입 금액은 1,000원입니다.");
+        if (amount % 1000 != 0) throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
     }
 
 }
