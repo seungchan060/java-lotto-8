2285d5fd24080a633107d03def1ca4b0c2378e52
SeungChan <dlatmdcks60@gmail.com>
2025-10-30 16:36:31 +0900

    feat(LottoValidator): 입력값 정수처리 및 예외처리

--- src/main/java/lotto/LottoValidator.java
+++ src/main/java/lotto/LottoValidator.java
@@ -1,4 +1,17 @@
 package lotto;
 
 public class LottoValidator {
+    public static int validatePurchaseAmount(String raw) {
+        int amount = parseInt(raw);
+        return amount;
+    }
+
+    private static int parseInt(String raw) {
+        try {
+            return Integer.parseInt(raw.trim());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 정수여야 합니다.");
+        }
+    }
+
 }
