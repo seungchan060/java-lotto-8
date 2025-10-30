fd1aa0e1b937040205c434425db214c5b98aed40
SeungChan <dlatmdcks60@gmail.com>
2025-10-30 16:13:56 +0900

    feat(input): 구입 금액 입력 받기
    - 로또 구입금액 받기

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -2,4 +2,9 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class InputView {}
+public class InputView {
+    public String readPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+}
