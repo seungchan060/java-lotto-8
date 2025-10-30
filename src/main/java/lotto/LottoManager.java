1c0c719dfab05a169a07c08120094f7ad5426e38
SeungChan <dlatmdcks60@gmail.com>
2025-10-30 17:24:28 +0900

    feat(LottoValidator): 로또 한 장의 가격 상수 PRICE_PER_TICKET 추가

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -1,4 +1,9 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class LottoManager {
+    private static final int PRICE_PER_TICKET = 1000;
 }
