e7fbfeafe6f2131d338738973ce898c7c02d71fd
SeungChan <dlatmdcks60@gmail.com>
2025-10-31 18:29:31 +0900

    feat(WinningLotto): 당첨 번호 및 보너스 번호 저장을 위한 WinningLotto 클래스 추가
    - 당첨 번호(numbers)와 보너스 번호(bonus)를 인스턴스 변수로 선언
    - 생성자를 통해 두 번호를 초기화하는 기본 구조 구현
    - numbers를 List.copyOf()를 사용하여 불변화하여 데이터 안전성 확보

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -1,4 +1,14 @@
 package lotto;
 
+import java.util.List;
+
 public class WinningLotto {
+    private final List<Integer> numbers;
+    private final int bonus;
+
+    public WinningLotto(List<Integer> numbers, int bonus) {
+        this.numbers = List.copyOf(numbers); // 불변화
+        this.bonus = bonus;
+    }
+
 }
