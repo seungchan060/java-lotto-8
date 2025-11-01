5cd1f986bcdb2b60d7c6dcc0a4d7ec0634ae7fd2
SeungChan <dlatmdcks60@gmail.com>
2025-11-01 17:39:36 +0900

    feat(WinningLotto): WinningLotto의 validate() 메서드 코드 스타일 개선

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -14,24 +14,14 @@ public class WinningLotto {
     }
 
     private void validate(List<Integer> nums, int bonus) {
-        if (nums == null || nums.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
-        }
+        if (nums == null || nums.size() != 6) throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
         Set<Integer> set = new HashSet<>(nums);
-        if (set.size() != nums.size()) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없습니다.");
-        }
+        if (set.size() != nums.size()) throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없습니다.");
         for (int n : nums) {
-            if (n < 1 || n > 45) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-            }
+            if (n < 1 || n > 45) throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
-        if (bonus < 1 || bonus > 45) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이여야 합니다.");
-        }
-        if (set.contains(bonus)) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
-        }
+        if (bonus < 1 || bonus > 45)  throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이여야 합니다.");
+        if (set.contains(bonus)) throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
     }
 
 }
