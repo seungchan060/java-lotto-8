9e34a137c0b255a5b99bfdb1c5d8c244e29d0a2a
SeungChan <dlatmdcks60@gmail.com>
2025-11-01 18:05:42 +0900

    refactor(WinningLotto): 생성자에서 validate()를 호출하지 않음을 해결

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -9,6 +9,7 @@ public class WinningLotto {
     private final int bonus;
 
     public WinningLotto(List<Integer> numbers, int bonus) {
+        validate(numbers, bonus);
         this.numbers = List.copyOf(numbers); // 불변화
         this.bonus = bonus;
     }
