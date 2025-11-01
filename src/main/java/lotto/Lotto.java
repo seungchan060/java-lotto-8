21219660ead5965406959048279132d7d4a08210
SeungChan <dlatmdcks60@gmail.com>
2025-11-01 18:12:50 +0900

    refactor(Lotto): validate() 조건 추가
    - 로또 번호 중복
    - 로또 번호 1~45 숫자 허용

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,8 +11,10 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        if (numbers.size() != 6) throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        if (new java.util.HashSet<>(numbers).size() != numbers.size())  throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        for (int n : numbers) {
+            if (n < 1 || n > 45) throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 
